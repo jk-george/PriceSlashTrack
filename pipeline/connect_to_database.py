ad5fee2c7626bf5647daeba9cf23a76d9499172d
gem09lo <gemmaw09lo@gmail.com>
2024-12-03 21:13:25 +0000

    added test files and type hints

--- pipeline/connect_to_database.py
+++ pipeline/connect_to_database.py
@@ -3,6 +3,8 @@ from os import environ
 import logging
 import psycopg2
 import psycopg2.extras
+from psycopg2.extensions import connection
+from psycopg2.extras import DictCursor
 from dotenv import load_dotenv
 
 
@@ -14,35 +16,36 @@ def configure_logging() -> None:
     )
 
 
-def get_connection():
-    """Connect to RDS database"""
+def get_connection() -> connection:
+    """Returns connection object"""
     try:
-        connection = psycopg2.connect(
+        conn = psycopg2.connect(
             user=environ["DB_USER"],
             password=environ["DB_PASSWORD"],
             host=environ["DB_HOST"],
             port=environ["DB_PORT"],
             database=environ["DB_NAME"]
         )
-        print("Connected to RDS database")
-        return connection
+        logging.info("Connected to the database successfully.")
+        return conn
     except psycopg2.Error as e:
-        print("Error connecting to the database:", e)
+        logging.error(f"Error connecting to the database: {e}")
         raise
 
 
-def get_cursor(connection):
-    """Returns cursor"""
+def get_cursor(conn: connection) -> DictCursor:
+    """Returns cursor object"""
     try:
-        return connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        return conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     except psycopg2.Error as e:
-        print("Error creating cursor:", e)
+        logging.error(f"Error creating cursor: {e}")
         raise
 
 
 if __name__ == "__main__":
 
     load_dotenv()
+    configure_logging()
 
-    conn = get_connection()
-    get_cursor(conn)
+    connection = get_connection()
+    get_cursor(connection)

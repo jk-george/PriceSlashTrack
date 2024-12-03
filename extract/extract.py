b85f1d512c3a41c2925d5cd4d9138dcce3bd10aa
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-12-03 11:38:33 +0000

    Created extract and test_extract files. Still being written in

---
+++ extract/extract.py
@@ -0,0 +1,22 @@
+"""
+Extraction script to find all sales tracking data we need from subscribed URL pages.
+
+1. Connect to RDS
+2. Query all URLs from the RDS
+3. Scrape the URL for: product_name,original_price,discount_price
+
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def extract_urls_from_db() -> list[str]:
+    """Function to get urls from a database"""
+    ...
+
+
+def get_html_from_url(web_page: str) -> bytes:
+    """ Gets the html content from a given URL"""
+    html = requests.get(web_page)
+    return html.content
---
+++ extract/test_extract.py
@@ -0,0 +1,8 @@
+from extract import scrape_from_url
+
+import pytest
+from unittest.mock import MagicMock
+
+
+@
+def test_scraper_gets_correct_steam_original_and_discount_price():

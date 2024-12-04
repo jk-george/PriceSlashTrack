90e70004451911407e49b309bdf67d457c3b85d1
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-12-04 13:30:31 +0000

    Created task definition and an iam role for the etl pipeline

--- terraform/terraform-etl-iterator/main.tf
+++ terraform/terraform-etl-iterator/main.tf
@@ -7,10 +7,114 @@ resource "aws_ecr_repository" "ecr_for_etl" {
   name                 = var.ETL_ECR_NAME  
   image_tag_mutability = "MUTABLE"  
   image_scanning_configuration {
-    scan_on_push = true  
+    scan_on_push = true
   }
+  force_delete = true
 
   lifecycle {
-    prevent_destroy = false  
+    prevent_destroy = false
   }
 }
+
+
+
+
+resource "aws_iam_role" "ecs_role" {
+  name               = "c14-priceslashers-ETL-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = ""
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect    = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_full_access" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_full_access" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+
+
+resource "aws_ecs_task_definition" "etl-task-def" {
+  family                   = "c14-priceslashers-ETL-taskdef"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_role.arn
+  task_role_arn            = aws_iam_role.ecs_role.arn
+  container_definitions    = jsonencode([
+    {
+      name      = var.ETL_ECR_NAME
+      image     = format("%s:latest", aws_ecr_repository.ecr_for_etl.repository_url)
+      essential = true
+      cpu       = 256
+      memory    = 512
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        },
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+      environment = [
+        {
+          name  = "DB_HOST"
+          value = var.DB_HOST
+        },
+        {
+          name  = "DB_USER"
+          value = var.DB_USER
+        },
+        {
+          name  = "DB_PASSWORD"
+          value = var.DB_PASSWORD
+        },
+        {
+          name  = "DB_NAME"
+          value = var.DB_NAME
+        },
+        {
+          name  = "DB_PORT"
+          value = var.DB_PORT
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/connect4-ETL-task"
+          "awslogs-region"        = "eu-west-2"
+          "awslogs-stream-prefix" = "ecs"
+          "awslogs-create-group"  = "true"
+        }
+      }
+    }
+  ])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
+
--- terraform/terraform-etl-iterator/variables.tf
+++ terraform/terraform-etl-iterator/variables.tf
@@ -3,3 +3,28 @@ variable ETL_ECR_NAME {
     type = string
     description = "ECR name for ETL process"
 }
+
+variable DB_HOST {
+    type = string
+    description = "Host address for database"
+}
+
+variable DB_PORT {
+    type = string
+    description = "Port for the database"
+}
+
+variable DB_PASSWORD {
+    type = string
+    description = "Password for database login"
+}
+
+variable DB_USER {
+    type = string
+    description = "Username for database"
+}
+
+variable DB_NAME {
+    type = string
+    description = "Name of the database"
+}

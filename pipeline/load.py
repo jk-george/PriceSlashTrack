1c0b0675649a9d31fda221c2c9f703ff10b61c16
gem09lo <gemmaw09lo@gmail.com>
2024-12-04 17:44:20 +0000

    pylinted

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,14 +1,8 @@
 """Uploads current price data to the RDS database triggered every 3 minutes"""
-import pandas as pd
-from os import environ
 import logging
-import psycopg2
-import psycopg2.extras
 from psycopg2.extensions import connection
-from psycopg2.extras import DictCursor
-from datetime import datetime
 from dotenv import load_dotenv
-from connect_to_database import get_connection, get_cursor
+from connect_to_database import get_connection
 
 
 def configure_logging() -> None:
@@ -18,19 +12,6 @@ def configure_logging() -> None:
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
-
-# def calculate_discount_percentage(original_price: float, current_price: float) -> float:
-#     """Calculates the discount percentage for the subscription table"""
-#     if original_price <= 0:
-#         logging.warning(f"""Invalid original price: {
-#                         original_price}. Returning 0% discount.""")
-#         return 0.0
-#     return max(0, ((original_price - current_price) / original_price) * 100)
-
-
-# def get_current_timestamp() -> str:
-#     """Generates the current timestamp in UTC format"""
-#     return datetime.utcnow().isoformat()
 
 def insert_price_change(conn: connection, product_id: int, price: float, timestamp: str) -> None:
     """Inserts values into price_changes schema"""
@@ -43,43 +24,9 @@ def insert_price_change(conn: connection, product_id: int, price: float, timesta
                 """,
                 (price, product_id, timestamp)
             )
-        logging.info(f"Inserted price for product_id {product_id}")
+        logging.info("Inserted price for product_id %s", product_id)
     except Exception as e:
-        logging.error(f"Error inserting price: {e}")
-
-# def update_subscription_discount(conn: connection, product_id: int, discount_percentage: float) -> None:
-#     """Updates the discount percentage in the subscription table."""
-#     try:
-#         with conn.cursor() as cur:
-#             cur.execute(
-#                 """
-#                 UPDATE subscription
-#                 SET discount_percentage = %s
-#                 WHERE product_id = %s;
-#                 """,
-#                 (discount_percentage, product_id)
-#             )
-#         logging.info(
-#             f"Updated subscription discount for product_id {product_id}.")
-#     except Exception as e:
-#         logging.error(f"Error updating subscription discount: {e}")
-
-
-# def load_price_changes(products_data: list[dict], conn: connection) -> None:
-#     """Loads cleaned price data into database"""
-#     try:
-#         for product in products_data:
-#             insert_price_change(
-#                 conn,
-#                 product["product_id"],
-#                 product["price"],
-#                 product["timestamp"]
-#             )
-#         conn.commit()
-#         logging.info("Data loaded successfully")
-#     except Exception as e:
-#         conn.rollback()
-#         logging.error(f"Load error: {e}")
+        logging.error("Error inserting price: %s", e)
 
 
 def load_price_changes(products_data: list[dict], conn: connection) -> None:
@@ -88,6 +35,10 @@ def load_price_changes(products_data: list[dict], conn: connection) -> None:
 
     for product in products_data:
         try:
+            if not all(key in product for key in ["product_id", "price", "timestamp"]):
+                logging.error("Invalid product values: %s", product)
+                continue
+
             insert_price_change(
                 conn,
                 product["product_id"],
@@ -96,30 +47,40 @@ def load_price_changes(products_data: list[dict], conn: connection) -> None:
             )
             successfully_inserted += 1
         except Exception as e:
-            logging.error(f"""Failed to insert product: {
-                          product['product_id']}. Error: {e}""")
+            logging.error(
+                "Failed to insert product: %s. Error: %s", product, e)
 
     try:
         if successfully_inserted > 0:
             conn.commit()
-            logging.info(f"""Data loaded successfully. {
-                         successfully_inserted}/{len(products_data)} rows committed.""")
+            logging.info(
+                "Data loaded successfully. %s rows committed.", successfully_inserted)
         else:
             raise Exception("No valid rows to commit.")
     except Exception as commit_error:
         conn.rollback()
-        logging.error(f"Load error during commit: {commit_error}")
+        logging.error("Load error during commit: %s", commit_error)
+
+
+def main_load():
+    """Main function for loading price_changes values"""
+    connection = get_connection()
+    try:
+        cleaned_data = [
+            {"price": 22.49, "timestamp": "2024-12-04 16:31:40"},
+            {"product_id": 9, "price": 7.69, "timestamp": "2024-12-04 16:31:40"},
+            {"product_id": 5, "price": 129.99,
+             "timestamp": "2024-12-04 16:31:40"}
+        ]
+
+        load_price_changes(cleaned_data, connection)
+    finally:
+        connection.close()
+        logging.info("Connection to database closed.")
 
 
 if __name__ == "__main__":
-
     load_dotenv()
     configure_logging()
 
-    connection = get_connection()
-    cursor = get_cursor(connection)
-    cleaned_data = [{'product_id': 8, 'timestamp': '2024-12-04 16:31:40'}, {'price': 7.69, 'product_id': 9,
-                                                                            'timestamp': '2024-12-04 16:31:40'}, {'price': 129.99, 'product_id': 5, 'timestamp': '2024-12-04 16:31:40'}]
-
-    load_price_changes(cleaned_data, connection)
-    connection.close()
+    main_load()
--- pipeline/test_load.py
+++ pipeline/test_load.py
@@ -1,59 +1,8 @@
+# pylint: skip-file
 import unittest
 from unittest.mock import Mock, patch, MagicMock
 from datetime import datetime
 from load import insert_price_change, load_price_changes
-
-
-# @patch("load.datetime")
-# def test_get_current_timestamp(mock_datetime):
-#     """Test current timestamp generation."""
-#     mock_datetime.utcnow.return_value = datetime(2024, 11, 9, 18, 0)
-#     result = get_current_timestamp()
-#     assert result == "2024-11-09T18:00:00"
-
-
-# def test_calculate_discount_percentage_valid():
-#     """Test valid discount percentage calculation."""
-#     original_price = 100.0
-#     current_price = 75.0
-#     result = calculate_discount_percentage(original_price, current_price)
-#     assert result == 25.0
-
-
-# def test_calculate_discount_percentage_invalid():
-#     """Test discount calculation with invalid original price."""
-#     original_price = 0
-#     current_price = 50.0
-#     result = calculate_discount_percentage(original_price, current_price)
-#     assert result == 0.0
-
-
-# @patch("load.logging.error")
-# def test_insert_price_change_failure(mock_log_error):
-#     """Test price change insertion failure."""
-#     mock_conn = MagicMock()
-#     mock_conn.cursor.side_effect = Exception("Insertion error")
-#     try:
-#         insert_price_change(mock_conn, product_id=1,
-#                             price=50.0, timestamp="2024-11-09T18:00:00")
-#     except Exception:
-#         pass
-#     mock_log_error.assert_called_once_with(
-#         "Error inserting price change: Insertion error")
-
-
-# @patch("load.logging.error")
-# def test_update_subscription_discount_failure(mock_log_error):
-#     """Test subscription discount update failure."""
-#     mock_conn = MagicMock()
-#     mock_conn.cursor.side_effect = Exception("Update error")
-#     try:
-#         update_subscription_discount(
-#             mock_conn, product_id=1, discount_percentage=20.0)
-#     except Exception:
-#         pass
-#     mock_log_error.assert_called_once_with(
-#         "Error updating subscription discount: Update error")
 
 
 def test_insert_price_change_valid():

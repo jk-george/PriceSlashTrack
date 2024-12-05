093135cdc842d70e7d576eeb9c7154b30730b397
Anita Megarry <anitamegarry@gmail.com>
2024-12-05 11:36:35 +0000

    Added functionality to add products to database

--- streamlit_login.py
+++ streamlit_login.py
@@ -1,11 +1,120 @@
 import streamlit as st
+import logging
 
+import requests
+import psycopg2
+from bs4 import BeautifulSoup
 from streamlit_graphs import get_connection, get_cursor
 
 headerSection = st.container()
 mainSection = st.container()
 loginSection = st.container()
 logOutSection = st.container()
+
+
+def get_html_from_url(web_page: str) -> bytes:
+    """ Gets the html content from a given URL"""
+    try:
+        html = requests.get(web_page, timeout=20)
+    except requests.exceptions.MissingSchema:
+        return "That URL does not exist."
+    except requests.exceptions.ConnectionError:
+        return "Cannot connect to that URL."
+    if html.status_code > 299 or html.status_code < 200:
+        return f"Error: {html.status_code}."
+    return html.content
+
+
+def get_website_from_url(url: str) -> str:
+    """ Gets a main website address from a given URL """
+    website_url = url
+    if ".com" in url:
+        website_url = url.split(".com")[0] + ".com"
+    elif ".co.uk" in url:
+        website_url = url.split(".co.uk")[0] + ".co.uk"
+    return website_url
+
+
+def scrape_from_html(html_content: bytes, url: str) -> dict:
+    """Scrapes from html to get a dictionary with the:
+    - product_name
+    - original_price
+    - website 
+    """
+    s = BeautifulSoup(html_content, 'html.parser')
+
+    results = s.find(id="game_area_purchase")
+
+    if not results:
+        logging.error("Can't scrape that URL.")
+        return None
+
+    original_price = results.find_all(
+        "div", class_="discount_original_price")[0]
+    game_title = s.find(
+        id="appHubAppName", class_="apphub_AppName")
+
+    product_information = {"game_title": game_title.text,
+                           "original_price": original_price.text,
+                           "website": get_website_from_url(url)}
+
+    return product_information
+
+
+def get_website_id(website: str) -> int:
+    """Checks if website exists and returns website id"""
+    try:
+        conn = get_connection()
+        cursor = get_cursor(conn)
+        query = "SELECT website_id FROM website WHERE website_name = %s"
+        cursor.execute(query, (website,))
+        result = cursor.fetchone()
+        return result[0] if result else None
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+        return None
+
+
+def get_user_id(email: str) -> int:
+    """Checks if user exists and returns user_id"""
+    try:
+        conn = get_connection()
+        cursor = get_cursor(conn)
+        query = "SELECT user_id FROM users WHERE email_address = %s"
+        cursor.execute(query, (email,))
+        result = cursor.fetchone()
+        return result[0] if result else None
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+        return None
+
+
+def get_product_id(url: str) -> int:
+    """Checks if product exists and returns product_id"""
+    try:
+        conn = get_connection()
+        cursor = get_cursor(conn)
+        query = "SELECT product_id FROM product WHERE url = %s"
+        cursor.execute(query, (url,))
+        result = cursor.fetchone()
+        return result[0] if result else None
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+        return None
+
+
+def get_subscription_id(user_id: str, product_id: int) -> int:
+    """Checks if subscription exists and returns subscription_id"""
+    try:
+        conn = get_connection()
+        cursor = get_cursor(conn)
+        query = "SELECT subscription_id FROM subscription WHERE user_id = %s and product_id = %s"
+        cursor.execute(query, (user_id, product_id,))
+        result = cursor.fetchone()
+        return result[0] if result else None
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+        return None
 
 
 def login(email: str, password: str) -> bool:
@@ -50,35 +159,84 @@ def create_account(first_name, last_name, new_email, new_password) -> bool:
         return False
 
 
-def track_product(product_name, url, original_price, notification_price):
-    """"""
+def insert_into_website(website: str) -> int:
+    """Inserts new websites into the website table and returns the corresponding website id"""
     try:
         conn = get_connection()
         cursor = get_cursor(conn)
-        query = """
-            INSERT INTO website (website_name) VALUES (%s);
-            INSERT INTO product (product_name, url, website_id, original_price) 
-            VALUES (%s, %s, %s, %s);
-            INSERT INTO subscription (user_id, product_id, notification_price) VALUES (
-            %s, %s, %s)"""
-        cursor.execute(query, ())
-        cursor.close()
-        conn.commit()
-        conn.close()
-        return True
+        if get_website_id(website):
+            return get_website_id(website)
+        else:
+            cursor.execute(
+                "INSERT INTO website (website_name) VALUES (%s);", (website,))
+            cursor.close()
+            conn.commit()
+            conn.close()
+            return get_website_id(website)
     except Exception as e:
         st.error(f"Error inserting into the database: {e}")
-        return False
+        return None
+
+
+def insert_into_product(website_id: int, url: str) -> int:
+    """Inserts new products into the product table and returns the corresponding product id"""
+    product_info = scrape_from_html(get_html_from_url(url), url)
+    try:
+        conn = get_connection()
+        cursor = get_cursor(conn)
+        if get_product_id(url):
+            return get_product_id(url)
+        else:
+            cursor.execute(
+                """INSERT INTO product (product_name, url, website_id, original_price) VALUES (%s, %s, %s, %s);""",
+                (product_info.get("game_title"), url, website_id, product_info.get("original_price"),))
+            cursor.close()
+            conn.commit()
+            conn.close()
+            return get_website_id(url)
+    except Exception as e:
+        st.error(f"Error inserting into the database: {e}")
+        return None
+
+
+def insert_into_subscription(user_id, product_id, notification_price):
+    """Inserts new subscriptions into the subscription table and returns the corresponding product id"""
+    try:
+        conn = get_connection()
+        cursor = get_cursor(conn)
+        if get_subscription_id(user_id, product_id):
+            return get_subscription_id(user_id, product_id)
+        else:
+            cursor.execute(
+                """INSERT INTO subscription (user_id, product_id, notification_price) VALUES (%s, %s, %s);""",
+                (user_id, product_id, notification_price,))
+            cursor.close()
+            conn.commit()
+            conn.close()
+            return get_subscription_id(user_id, product_id)
+    except Exception as e:
+        st.error(f"Error inserting into the database: {e}")
+        return None
+
+
+def track_product(user_id, url, notification_price):
+    """Inserts product details into the database"""
+    try:
+        website_id = insert_into_website(get_website_from_url(url))
+        product_id = insert_into_product(website_id, url)
+        insert_into_subscription(user_id, product_id, notification_price)
+    except Exception as e:
+        st.error(f"Error inserting into the database: {e}")
 
 
 def show_main_page():
     with mainSection:
         st.header("Track a new product")
-        product = st.text_input("Enter a new product URL: ")
+        url = st.text_input("Enter a new product URL: ")
         notification_price = st.text_input(
             label="Enter the price threshold to receive email notifications about price drops: ")
         st.button("Track", on_click=track_clicked,
-                  args=(product, notification_price))
+                  args=(url, notification_price))
         processingClicked = st.button("Start Processing", key="processing")
         if processingClicked:
             st.balloons()
@@ -98,6 +256,8 @@ def show_logout_page():
 def login_clicked(email, password):
     if login(email, password):
         st.session_state['loggedIn'] = True
+        st.session_state['user_id'] = get_user_id(email)
+        st.toast("Login successful!")
     else:
         st.session_state['loggedIn'] = False
         st.error("Invalid user name or password")
@@ -110,13 +270,18 @@ def create_account_clicked(first_name, last_name, new_email, new_password):
         st.session_state['loggedIn'] = False
     elif create_account(first_name, last_name, new_email, new_password):
         st.session_state['loggedIn'] = True
+        st.session_state['user_id'] = get_user_id(new_email)
+        st.toast("Account successfully created!")
     else:
         st.session_state['loggedIn'] = False
         st.error("Error occurred when creating account")
 
 
-def track_clicked():
-    ...
+def track_clicked(user_id, url, notification_price):
+    if not url or not notification_price:
+        st.error("All fields are required to track a product.")
+    else:
+        track_product(user_id, url, notification_price)
 
 
 def show_login_page():

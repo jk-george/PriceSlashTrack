a788537160c6fd77ade13ac411e270d8cb6ad116
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-12-09 11:24:39 +0000

    Created streamlit dashboard requirements file.  Created a dashboard dockerfile. Added provider block to ecs terraform

---
+++ streamlit_dashboard_upload.sh
@@ -0,0 +1,8 @@
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
+
+docker build --platform linux/amd64 -f streamlit_dashboard.dockerfile -t c14-priceslashers-dashboard-ecr .
+
+docker tag c14-priceslashers-dashboard-ecr:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c14-priceslashers-dashboard-ecr:latest
+
+docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c14-priceslashers-dashboard-ecr:latest
+
